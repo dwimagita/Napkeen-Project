2474e86d1c603acf5da2e73692aa36bad5f8d649
Presi <presinillaalfrista97@gmail.com>
2018-03-05 12:12:51 +0700

    test

--- app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/Home.java
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/Home.java
@@ -1,13 +1,37 @@
 package com.example.imadedwimagitadirtana_1202150054_si3906.napkeen;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.content.Intent;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -23,27 +47,41 @@ public class Home extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
+        setNavigationViewListener();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        toolbar.setLogo(R.drawable.napkeennlogoforhome);
+
+
 
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.addTab(tabLayout.newTab().setText("Dessert"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
         final PagerAdapter adapter = new PagerAdapter
                 (getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
 
 
+
+
         viewPager.addOnPageChangeListener(new
                 TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
@@ -58,5 +96,83 @@ public class Home extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent home = new Intent(Home.this, Home.class);
+                startActivity(home);
+                break;
+            case R.id.nav_nearby:
+
+                break;
+            case R.id.nav_tempat_terbaik:
+                //Do some thing here
+                // add navigation drawer item onclick method here
+                break;
+            case R.id.nav_bantuan:
+                //Do some thing here
+                // add navigation drawer item onclick method here
+                break;
+            case R.id.nav_tentang:
+                //Do some thing here
+                // add navigation drawer item onclick method here
+                break;
+            case R.id.nav_tambah_restoran:
+
+                break;
+            case R.id.nav_pengaturan:
+                //Do some thing here
+                // add navigation drawer item onclick method here
+                break;
+            case R.id.nav_Keluar:
+                //Do some thing here
+                // add navigation drawer item onclick method here
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item =  menu.findItem(R.id.action_search);
+        SearchView searchView =(SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
+
+
+
+
+
+
+
+
