56e2ddbacce3e7c708fd33338f3f5b5b1f47e428
dwima <dwima97@gmail.com>
2018-03-19 09:29:26 +0700

    pushh 19/03/2018

--- app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/MainActivity.java
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.imadedwimagitadirtana_1202150054_si3906.napkeen;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                    finish();
+                }
+            }
+        };
     }
 
     public void launchLoginActivity(View view) {
@@ -24,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            mAuth.removeAuthStateListener(authListener);
+        }
+    }
 
 }
