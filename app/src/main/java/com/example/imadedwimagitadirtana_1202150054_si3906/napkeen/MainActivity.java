ffc224c148c5461881fcc3f16dc5753f2f57a0eb
fanharilardian <fanharilfanharil@gmail.com>
2018-04-02 12:51:48 +0700

    Merge branch 'master' of https://github.com/dwimagita/Napkeen-Project into fanharil
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/MainActivity.java
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/MainActivity.java
@@ -15,8 +15,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener authListener;
 
@@ -24,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         mAuth = FirebaseAuth.getInstance();
         //get current user
