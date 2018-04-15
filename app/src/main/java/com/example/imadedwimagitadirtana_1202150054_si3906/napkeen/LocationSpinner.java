b23d8738c1dd3e20cc11da3ff249f47e85749474
fanharilardian <fanharilfanharil@gmail.com>
2018-04-15 21:28:56 +0700

    Merge branch 'master' of https://github.com/dwimagita/Napkeen-Project into fanharil
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    - Upload setelah Cleaning Project!

---
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/napkeen/LocationSpinner.java
@@ -0,0 +1,13 @@
+package com.example.imadedwimagitadirtana_1202150054_si3906.napkeen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_spinner);
+    }
+}
